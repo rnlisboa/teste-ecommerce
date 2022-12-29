433ed4f28bc7693390ea2e7eed247f4acff6a9d4
ren4nl1sb <dev.renanlisboa@gmail.com>
2022-12-29 10:27:40 -0300

    criando models para produtos e carrinho

--- backend/loja/settings.py
+++ backend/loja/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'produtos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'produtos'
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- backend/produtos/models.py
+++ backend/produtos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Product(models.Model):
+    product_image = models.FileField(
+        upload_to=r'produtos/%Y/%m/%d/', verbose_name='Imagem')
+    product_name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Produto')
+    product_description = models.TextField(
+        blank=False, null=False, verbose_name='Descrição')
+    product_price = models.DecimalField(
+        max_digits=9, decimal_places=2, null=False, blank=False)
+
+    def __str__(self):
+        return self.product_name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    products = models.ManyToManyField(Product, verbose_name='Produtos')
+    total_price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name='Preço total')
